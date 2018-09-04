3b5649c87d201642e84ff325fc81348b72c7c591
marb61a <marb61a@gmail.com>
2018-09-04 16:39:19 +0100

    added Stripes.js and PromotionAnimation.js component files

---
+++ src/Components/home/featured/Stripes.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Stripes extends Component {
+  state = { 
+    stripes: [
+
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default Stripes;
---
+++ src/Components/home/promotion/Animation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PromotionAnimation = () => { 
+  return (
+    <div className="promotion_animation">
+      <div className="left">
+      
+      </div>
+      <div className="right">
+      
+      </div>      
+    </div>
+  )
+};
+
+export default PromotionAnimation;
