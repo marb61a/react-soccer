ff8e762a325748d09f03556e3d19b5100e042c09
marb61a <marb61a@gmail.com>
2018-09-05 17:22:47 +0100

    added animateSecond function to featured/Text.js

--- src/Components/home/featured/Text.js
+++ src/Components/home/featured/Text.js
@@ -65,6 +65,37 @@ class Text extends Component {
     </Animate>
   );
 
+  animateSecond = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity:0,
+        x:503,
+        y:586
+      }}
+      enter={{
+        opacity:[1],
+        x:[273],
+        y:[586],
+        timing:{delay:300,duration: 500, ease:easePolyOut}
+      }}
+    >
+    { ({ opacity, x, y }) => {
+      return(
+        <div 
+          className="featured_second"
+          style={{
+              opacity,
+              transform: `translate(${x}px,${y}px)`
+          }}
+        >
+          Championships
+        </div>
+      ) 
+    } }
+    </Animate>
+  )
+
   animatePlayer = () => (
     <Animate
       show={true}
