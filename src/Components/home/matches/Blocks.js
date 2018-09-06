106dfe10c35558cf93d1923de66f977a2c57ff0f
marb61a <marb61a@gmail.com>
2018-09-06 11:50:02 +0100

    updated matches_block ui component and Blocks.js component

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
+
+import MatchesBlock from '../../ui/matches_block';
 
 class Blocks extends Component {
+  state = {
+    matches: []
+  };
+
+  componentDidMount(){
+
+  }
+
+  showMatches = (matches) => (
+    matches ? 
+    matches.map() :
+    null 
+  );
+
   render() {
     return (
-      <div>
-        
+      <div className="home_matches">
+        { this.showMatches(this.state.matches) }
       </div>
     );
   }
--- src/Components/ui/icons.js
+++ src/Components/ui/icons.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import mutdlogo from '../../Resources/images/logos/Manchester_United_logo_logotype_crest.png';
+import mcitylogo from '../../Resources/images/logos/Manchester_United_logo_logotype_crest.png';
 
 export const UtdLogo = (props) => {
-
-    const template = <div
-        className="img_cover"
-        style={{
-            width: props.width,
-            height: props.height,
-            background:`url(${mutdlogo}) no-repeat`
-        }}
-    ></div>
+  const template = <div
+    className="img_cover"
+    style={{
+    width: props.width,
+    height: props.height,
+    background:`url(${mcitylogo}) no-repeat`
+    }}
+  >
+  </div>
 
   if(props.link){
     return (
-        <Link to={props.linkTo} className="link_logo">
-            {template}
-        </Link>
-    )
+      <Link to={props.linkTo} className="link_logo">
+        { template }
+      </Link>
+    );
   } else {
-    return template
+    return template;
   }
-}
+};
