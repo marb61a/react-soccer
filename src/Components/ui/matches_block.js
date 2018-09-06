3882687b4ba042dab164a862da102126e8c55251
marb61a <marb61a@gmail.com>
2018-09-06 11:36:30 +0100

    updated matches_block ui component

--- src/Components/ui/matches_block.js
+++ src/Components/ui/matches_block.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 const MatchesBlock = ({match}) => {
   return (
-    <div>
-    
+    <div className="match_block">
+      <div className="match_date">
+        {
+          match.final ? match.date : `Match not played yet: ${match.date}`
+        }
+      </div>
+      <div className="match_wrapper">
+        <div className="match_top">
+          <div className="left">
+            <div
+              className="icon"
+              style={{
+                background:`url(/images/team_icons/${match.localThmb}.png)`
+              }}
+            >
+            </div>
+            <div className="team_name">
+              { match.local }
+            </div>
+          </div>
+          <div className="right">
+              { match.final ? match.finalResult : '-' }
+          </div> 
+        </div>
+        <div className="match_bottom">
+          <div className="left">
+          
+          </div>
+          <div className="right">
+          
+          </div>          
+        </div>
+      </div>
     </div>
   );
 }
