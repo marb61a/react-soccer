285562142c7b2a204cfa207aba15bfc8aabafc54
marb61a <marb61a@gmail.com>
2018-09-06 17:10:40 +0100

    updatemeetPlayers index component

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -44,6 +44,48 @@ class MeetPlayers extends Component {
                     Meet
                   </Tag>
                 </div>
+                <div>
+                  <Tag 
+                    bck="#0e1731" 
+                    size="100px" 
+                    color="#ffffff" 
+                    add={{
+                      display: 'inline-block',
+                      marginBottom: '20px',
+                    }}
+                  >
+                    The
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#0e1731" 
+                    size="100px" 
+                    color="#ffffff" 
+                    add={{
+                      display: 'inline-block',
+                      marginBottom: '20px'
+                    }}
+                  >
+                    Players  
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#0e1731" 
+                    size="27px"
+                    color="##0e1731"
+                    link={true}
+                    linkto="/the_team"
+                    add={{
+                      display: 'inline-block',
+                      marginBottom: '27px',
+                      border: '1px solid #0e1731'
+                    }}
+                  >
+                    Meet them here
+                  </Tag>
+                </div>
               </div>
             </div>
           </div>
