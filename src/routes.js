99b90c0b5c924d17f5efd26fbe54afbd18926f18
marb61a <marb61a@gmail.com>
2018-09-17 11:46:48 +0100

    added PublicRoutes component

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,17 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route }  from 'react-router-dom';
 
+import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
+
 import Home from './Components/home';
+import SignIn from './Components/signin';
+
+import Dashboard from './Components/admin/Dashboard';
+import AdminMatches from './Components/admin/matches';
+import AddEditMatch from './Components/admin/matches/addEditMatch';
+import AdminPlayers from './Components/admin/players';
+import AddEditPlayers from './Components/admin/players/addEditPlayers';
 
 const Routes = (props) => {
   return(
