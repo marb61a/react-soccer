e45dab8c85657b79bf9409d63807db7dc23bfdc7
marb61a <marb61a@gmail.com>
2018-09-17 12:09:34 +0100

    added Admin players index

---
+++ src/Components/admin/nav/AdminNav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ListItem from '@material-ui/core/ListItem';
+import { firebase } from '../../../firebase';
+
+const AdminNav = () => {
+  const links = [
+
+  ];
+
+  const style = {
+    color: '#ffffff',
+    fontWeight: '300',
+    borderBottom:'1px solid #353535'
+  };
+
+  return (
+    <div>
+    
+    </div>
+  );
+};
+
+export default AdminNav;
---
+++ src/Components/admin/players/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AdminLayout from '../../../Hoc/AdminLayout';
+
+class AdminPlayers extends Component {
+  state = {
+    isLoading: true,
+    players: []
+  };
+
+  render(){
+    return (
+      <AdminLayout>
+      
+      </AdminLayout>
+    )
+  }
+}
+
+export default AdminPlayers;
