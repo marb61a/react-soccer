b8d9b4bc43e6e312e9d0f9f6b3a5e947ceedc5b0
marb61a <marb61a@gmail.com>
2018-09-18 11:21:37 +0100

    updated admin nav

--- src/Components/admin/nav/AdminNav.js
+++ src/Components/admin/nav/AdminNav.js
@@ -5,7 +5,22 @@ import { firebase } from '../../../firebase';
 
 const AdminNav = () => {
   const links = [
-
+    {
+      title: 'Matches',
+      linkTo: '/admin_matches'
+    },
+    {
+      title: 'Add Match',
+      linkTo: '/admin_matches/edit_match'
+    },
+    {
+      title: 'Players',
+      linkTo: '/admin_players'
+    },
+    {
+      title: 'Add Players',
+      linkTo: '/admin_players/add_players'
+    }
   ];
 
   const style = {
@@ -14,9 +29,22 @@ const AdminNav = () => {
     borderBottom:'1px solid #353535'
   };
 
+  const renderItems = () => (
+    links.map(link => (
+      <Link to={link.linkTo} key={link.title}>
+        <ListItem button style={style}>
+          {link.title}
+        </ListItem>
+      </Link>
+    ))
+  );
+
   return (
     <div>
-    
+      { renderItems() }
+      <ListItem>
+        Logout
+      </ListItem>
     </div>
   );
 };
