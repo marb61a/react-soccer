4485ae408cb377990bea704f62eb27e3bad5ac97
marb61a <marb61a@gmail.com>
2018-09-18 11:59:54 +0100

    updated admin addEditPlayers

--- src/Components/admin/players/addEditPlayers.js
+++ src/Components/admin/players/addEditPlayers.js
@@ -11,13 +11,52 @@ class AddEditPlayers extends Component {
     formError: false,
     formSuccess:'',
     formdata: {
-
+      name: {
+        element:'input',
+        value:'',
+        config:{
+            label: 'Player Name',
+            name:'name_input',
+            type: 'text'
+        },
+        validation:{
+            required: true
+        },
+        valid: false,
+        validationMessage:'',
+        showlabel: true
+      },
+      lastname: {
+        element:'input',
+        value:'',
+        config:{
+            label: 'Player Last name',
+            name:'lastname_input',
+            type: 'text'
+        },
+        validation:{
+            required: true
+        },
+        valid: false,
+        validationMessage:'',
+        showlabel: true
+      }
     }
   };
 
   submitForm(event) {
     event.preventDefault();
     
+    let dataToSubmit = {};
+    let formIsValid = true;
+
+    if(formIsValid) {
+
+    } else {
+      this.setState({
+        formError: true
+      });
+    }
   }
 
   render(){
@@ -26,7 +65,9 @@ class AddEditPlayers extends Component {
         <div>
           <h2>AddEditPlayers</h2>
           <div>
-          
+            <form onSubmit={(event)=> this.submitForm(event)}>
+            
+            </form>
           </div>
         </div>
       </AdminLayout>
