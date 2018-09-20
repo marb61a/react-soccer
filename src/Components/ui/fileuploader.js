e57e5b2c997de1f07e849347e37a91376c06e2e4
marb61a <marb61a@gmail.com>
2018-09-20 11:32:38 +0100

    updated fileUploader ui component

--- src/Components/ui/fileuploader.js
+++ src/Components/ui/fileuploader.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import { firebase } from '../../firebase';
+import FileUploader from 'react-firebase-file-uploader';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class Fileuploader extends Component {
   state = {
-    name: ''
+    name: '',
+    isUploading: false
   };
+
+  handleUploadStart = () => {
+    this.setState({
+      isUploading: true
+    });
+  }
+
+  handleUploadError = () => {
+    this.setState({
+      isUploading: false
+    });
+  }
+
+  handleUploadSuccess = (filename) => {
+    console.log(filename);
+
+    this.setState({
+      name: filename,
+      isUploading: false
+    });
+
+  }
 
   render(){
     return (
       <div>
-        Uploader
+        {
+          this.state.fileURL ?
+          <div>
+            <div className="label_inputs">
+              {this.props.tag}
+            </div>
+            
+          </div> :
+          null
+        }
       </div>
     );
   }
