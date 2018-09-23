9cdda647ad737aef8b1e110bce5d02f9fd107e09
marb61a <marb61a@gmail.com>
2018-09-23 16:52:41 +0100

    added functions to fileuploader.js

--- src/Components/ui/fileuploader.js
+++ src/Components/ui/fileuploader.js
@@ -6,7 +6,8 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 class Fileuploader extends Component {
   state = {
     name: '',
-    isUploading: false
+    isUploading: false,
+    fileURL:''
   };
 
   handleUploadStart = () => {
@@ -29,6 +30,36 @@ class Fileuploader extends Component {
       isUploading: false
     });
 
+    firebase.storage().ref(this.props.dir)
+      .child(filename).getDownloadURL()
+      .then(url => {
+        this.setState({
+          fileURL: url
+        });
+      })
+
+      this.props.filename(filename);
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if(props.defaultImg) {
+      return state = {
+        name: props.defaultImgName,
+        fileURL: props.defaultImg
+      };
+    }
+
+    return null;
+  }
+
+  uploadAgain = () => {
+    this.setState({
+      name: '',
+      isUploading: false,
+      fileURL: ''
+    });
+
+    this.props.resetImage();
   }
 
   render(){
