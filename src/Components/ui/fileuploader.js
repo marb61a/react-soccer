e5662b77302237e7d03d3445645f71851b4a72e8
marb61a <marb61a@gmail.com>
2018-09-23 16:58:58 +0100

    updated fileuploader.js form

--- src/Components/ui/fileuploader.js
+++ src/Components/ui/fileuploader.js
@@ -71,7 +71,35 @@ class Fileuploader extends Component {
             <div className="label_inputs">
               {this.props.tag}
             </div>
-            
+            <FileUploader 
+              accept="image/*"
+              name="image"
+              randomizeFilename
+              storageRef={firebase.storage().ref(this.props.dir)}
+              onUploadStart={this.handleUploadStart}
+              onUploadError={this.handleUploadStart}
+              onUploadSuccess={this.handleUploadSuccess}
+            />
+          </div> :
+          null
+        }
+        {
+          this.state.isUploading ?
+          <div
+            className="progress"
+            style={{textAlign:'center',margin:'30px 0'}}
+          >
+            <CircularProgress 
+              style={{color:'#98c6e9'}}
+              thickness={7}
+            />
+          </div> :
+          null
+        }
+        {
+          this.state.fileURL ?
+          <div>
+          
           </div> :
           null
         }
