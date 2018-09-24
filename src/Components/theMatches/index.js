cb6136b1a64056d80932d905183b2756239916f5
marb61a <marb61a@gmail.com>
2018-09-24 20:29:56 +0100

    added functions to theMatches index component

--- src/Components/theMatches/index.js
+++ src/Components/theMatches/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { firebaseMatches } from '../../firebase';
 import { firebaseLooper, reverseArray } from '../ui/misc';
 
+import LeagueTable from './table';
+import MatchesList from './matchesList';
 class TheMatches extends Component {
   state = {
     loading: true,
@@ -16,8 +18,26 @@ class TheMatches extends Component {
   componentDidMount() {
     firebaseMatches.once('value')
       .then(snapshot => {
+        const matches = firebaseLooper(snapshot);
 
+        this.setState({
+          loading: false,
+          matches: reverseArray(matches),
+          filterMatches: reverseArray(matches)
+        });
       });
+  }
+
+  showPlayed = (played) => {
+    const list = this.state.matches.filter((match) => {
+      return match.final === played;
+    });
+
+    this.setState({
+      filterMatches: played === 'All' ? this.state.matches : list,
+      playedFilter: played,
+      resultFilter: 'All'
+    });
   }
 
   render() {
