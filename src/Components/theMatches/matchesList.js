675041e978d459ee79d17dc1fc88eef2b3a4e290
marb61a <marb61a@gmail.com>
2018-09-25 12:20:59 +0100

    updating matchesList form

--- src/Components/theMatches/matchesList.js
+++ src/Components/theMatches/matchesList.js
@@ -22,7 +22,11 @@ class MatchesList extends Component {
         opacity: 0,
         x: -200
       })}
-
+      enter={(d, i) => ({
+        opacity: [1],
+        x: [0],
+        timing:{duration: 500, delay: i * 50, ease: easePolyOut}
+      })}
     >
       {(nodes) => (
         <div>
@@ -37,7 +41,34 @@ class MatchesList extends Component {
             >
               <div className="block_wrapper">
                 <div className="block">
-                
+                  <div
+                    className="icon"
+                    style={{
+                      background:`url(/images/team_icons/${data.localThmb}.png)`
+                    }}
+                  >
+                  </div>
+                  <div className="team">
+                    { data.local }
+                  </div>
+                  <div className="result">
+                    { data.resultLocal }
+                  </div>
+                </div>
+                <div className="block">
+                  <div 
+                    className="icon" 
+                    style={{
+                      background:`url(/images/team_icons/${data.awayThmb}.png)`
+                    }}
+                  >
+                  </div>
+                  <div className="team">
+                    { data.away }
+                  </div>
+                  <div className="result">
+                    { data.resultAway }
+                  </div>
                 </div>
               </div>
             </div>
