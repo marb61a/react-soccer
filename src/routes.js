417959282339500a8370a04c207bfb17cc2eeeef
marb61a <marb61a@gmail.com>
2018-09-26 11:34:58 +0100

    updated index.js and added routes to routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,9 @@ import PublicRoute from './Components/authRoutes/publicRoutes';
 
 import Home from './Components/home';
 import SignIn from './Components/signin';
+import TheTeam from './Components/theTeam';
+import TheMatches from './Components/theMatches';
+import NotFound from './Components/ui/not_found';
 
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
@@ -70,9 +73,28 @@ const Routes = (props) => {
       <PublicRoute 
         {...props} 
         restricted={false} 
+        path="/the_matches" 
+        exact 
+        component={TheMatches}
+      />
+      <PublicRoute 
+        {...props} 
+        restricted={false} 
+        path="/the_team" 
+        exact 
+        component={TheTeam}
+      />
+      <PublicRoute 
+        {...props} 
+        restricted={false} 
         path="/" 
         exact 
         component={Home}
+      />
+      <PublicRoute 
+        {...props} 
+        restricted={false} 
+        component={NotFound}
       />
       </Switch>
     </Layout>
