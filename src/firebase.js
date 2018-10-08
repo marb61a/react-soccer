018bee5f46c827395ea3520a0ddf44fd75174beb
marb61a <marb61a@gmail.com>
2018-10-08 17:02:53 +0100

    added new image and updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/app';
 import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/storage';
 
 const config = {
   apiKey: "",
-  authDomain: "",
+  authDomain: "react-soccer.firebaseapp.com",
   databaseURL: "",
   projectId: "react-soccer",
   storageBucket: "",
@@ -16,9 +18,14 @@ firebase.initializeApp(config);
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
 const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseTeams = firebaseDB.ref('teams');
+const firebasePlayers = firebaseDB.ref('players');
 
 export {
     firebase,
     firebaseMatches,
-    firebasePromotions
+    firebasePromotions,
+    firebaseTeams,
+    firebasePlayers,
+    firebaseDB
 };
